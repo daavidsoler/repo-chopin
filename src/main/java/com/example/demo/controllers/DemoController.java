495db1f51e007c0e0c0a0aa3a39646b10b22a711
daavidsoler <davidsolerj03@gmail.com>
2025-10-03 16:06:12 +0200

    primer commit de models

--- src/main/java/com/example/demo/controllers/DemoController.java
+++ src/main/java/com/example/demo/controllers/DemoController.java
@@ -1,22 +1,39 @@
 package com.example.demo.controllers;
 
+import com.example.demo.model.MusicalPiece;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDate;
+import java.util.List;
+
+/*
+ * In this code, we define a Spring Boot controller with multiple routes,
+ * each route corresponds to a method that returns the name of the view to be rendered.
+ * The controller also adds attributes to the model to be used in the views.
+ */
 
 @Controller
 public class DemoController {
-    /**
-     * method to handle requests to the root, index, and home URLs.
-     * @return the name of the view to be rendered
-     */
+
     @GetMapping({"/", "/index", "/home"})
-    public String showIndex() {
+    public String showIndex(Model model) {
+        model.addAttribute("year", LocalDate.now().getYear());
         return "indexView";
     }
 
+
+    //here MusicalPiece is used to create a list of musical pieces and pass it to the view
     @GetMapping("/repertorio")
-    public String showRepertorio() {
+    public String showRepertorio(Model model) {
+        List<MusicalPiece> pieces = List.of(
+                new MusicalPiece("Nocturne Op.9 No.2", "Chopin", 1832, "Solo piano"),
+                new MusicalPiece("Piano Concerto No.1", "Chopin", 1830, "Piano concert"),
+                new MusicalPiece("Ballade No.1 in G minor", "Chopin", 1835, null)
+        );
+
+        model.addAttribute("pieces", pieces);
         return "repertorioView";
     }
 
@@ -25,10 +42,8 @@ public class DemoController {
         return "galeriaView";
     }
 
-   @GetMapping("/enlaces")
-   public String showEnlaces() {
-       return "enlacesView";
-   }
-   
-    
+    @GetMapping("/enlaces")
+    public String showEnlaces() {
+        return "enlacesView";
+    }
 }
---
+++ src/main/java/com/example/demo/model/MusicalPiece.java
@@ -0,0 +1,9 @@
+package com.example.demo.model;
+
+public record MusicalPiece(
+        String title,
+        String composer,
+        Integer year,
+        String instrumentation
+) {}
+
